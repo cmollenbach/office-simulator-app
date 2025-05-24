29e5ca97973408c9b1b2f3753893110a7a0ad9ee
cmollenbach <christoffer@mollenbach.com>
2025-05-24 07:37:08 +0200

    Initial commit: Setup React application

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the main application title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Office Seat Utilization Simulator/i);
+  expect(titleElement).toBeInTheDocument();
 });
